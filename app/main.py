c6b2555daa276d821bb7414ad56e9b9d5fcbd493
pydragon <137177056+pydragon1@users.noreply.github.com>
2023-11-02 01:27:19 +0700

    test1

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template,  redirect, request
 from dbworker import *
 # from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
-
 @app.route("/")
 def main_site():
-    return """<a href="/posts">Hllo</a>"""
+    return render_template("index.html")
+
+
 
 @app.route("/logging", methods=["GET", "POST"])
 def loging():
@@ -15,11 +16,14 @@ def loging():
         return "HEllo"
     return render_template("register.html")
 
+
+
 @app.route("/posts")
 def posts():
     data = DataBase.get_all_posts()
     col = len(data)
     return render_template("posts.html", col=col, data=data)
+
 
 @app.route("/posts/post<int:post_id>")
 def open_post(post_id):
@@ -33,7 +37,8 @@ def add_post():
         content = request.form.get("add_post_content")
         title = request.form.get("add_post_title")
         DataBase.add_posts(title, content)
-        return "Успешная публикация!!!"
+        return "Успешная Публикация"
+        # return render_template("PublicationDone.html")
     return render_template("add_post.html")
 
 # app.add_url_rule('/', 'index', main_site)
